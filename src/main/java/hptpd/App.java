f839309150d441b3fcbb9395a9a1c09219e1ae2d
yangjun7948 <yangjun7948@163.com>
2023-08-03 16:04:31 +0800

    添加环保212协议读取JAR包

---
+++ src/main/java/hptpd/App.java
@@ -0,0 +1,17 @@
+package hptpd;
+
+import com.hj212.format.hbt212.core.T212Mapper;
+import com.hj212.format.hbt212.exception.T212FormatException;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ * @author tracy4262
+ */
+public class App {
+    public static void main(String[] args) throws T212FormatException, IOException {
+        System.out.println("Hello World!");
+    }
+}
